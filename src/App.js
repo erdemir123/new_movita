2ee027b5ef9b0c50b401fef2f07750cf11069867
sadik <sadikerdemir123@icloud.com>
2023-11-21 16:26:53 +0300

    proje başlandı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from "../src/assets/movitaLogo.png"
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [show,setShow]=useState(true)
+  useEffect(()=>{
+    setTimeout(()=>setShow(false),2000)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {show && <div className="bg-black h-screen w-100 flex justify-center items-center overflow-hidden flex-col">
+      <AnimatePresence >
+          <motion.div
+            key={logo}
+            initial={{ y: 1000, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 0, opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <img src={logo} className="w-[600px]" alt=""/>
+          </motion.div>
+        </AnimatePresence>
+        <motion.h2  
+            className='text-orange-500 text-3xl font-extrabold'
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: [0, -20, 0], opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5}}
+            >
+              Movita Mobil Vasıta İzleme Takip sistemi
+        </motion.h2>
+    </div> }
+    {show || <div className="bg-white h-screen w-100 flex justify-center items-center overflow-hidden flex-col">
+      movita
+    </div> }
+    </>
   );
 }
 
